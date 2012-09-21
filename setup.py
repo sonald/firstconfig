1c224deb6ecd7c7d46c07ab197cb6063c13771c3
Sian Cao <sycao@redflag-linux.com>
2012-09-21 09:16:57 +0800

    redesign UI elements

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ data_files = [('/usr/sbin', ['rffirstconfig']),
         ('/usr/share/firstconfig/data/', glob('data/*.html')),
         ('/usr/share/firstconfig/data/css', glob('data/css/*.*')),
         ('/usr/share/firstconfig/data/js', glob('data/js/*.js')),
+        ('/usr/share/firstconfig/data/img', glob('data/js/*.png')),
         ('/usr/share/firstconfig/data/messages', glob('data/messages/*.*')),
         ('/usr/share/firstconfig/data/licenses', glob('data/licenses/*.html')),
         ('/usr/share/firstconfig/data/scripts', glob('data/scripts/*.*'))]
