05cd7cd414d3b7685e83466f4547cbbea5012058
孙志刚 <sunzhigang@redflag-linux.com>
2012-10-25 17:36:07 +0800

    rm /usr/lib/yum/uuid

--- data/scripts/setup.sh
+++ data/scripts/setup.sh
@@ -95,6 +95,7 @@ fi
 
 ## uninstall qomoinstaller
 rpm -e nodejs-hippo nodejs-hippo-deps nodejs rfconfig-boot
+test -f /usr/lib/yum/uuid && rm -f /usr/lib/yum/uuid
 
 if [ -n "$HIPPO_HOSTNAME" ]; then
     ## hostname
