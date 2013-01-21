691036c9adf0a91b54f94192e5f0033f09e28523
孙志刚 <sunzhigang@redflag-linux.com>
2013-01-21 17:54:30 +0800

    所有英文环境都删除fcitx

--- data/scripts/setup.sh
+++ data/scripts/setup.sh
@@ -66,6 +66,8 @@ _EOF
 
 fi
 
+[[ ${HIPPO_LANG} != "zh_CN.UTF-8" ]] && yum erase -q -y fcitx
+
 if [ -n "$HIPPO_LIVECD" ]; then
 
     # ping
@@ -107,7 +109,6 @@ fi
 ## uninstall qomoinstaller
 rpm -e nodejs-hippo nodejs-hippo-deps nodejs rfconfig-boot
 test -f /var/lib/yum/uuid && rm -f /var/lib/yum/uuid
-[[ ${HIPPO_LANG} != "zh_CN.UTF-8" ]] && yum erase -q -y fcitx
 
 if [ -n "$HIPPO_HOSTNAME" ]; then
     ## hostname
